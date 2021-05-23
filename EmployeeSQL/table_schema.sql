bda94ddfc0263b27216c10b1e4f2cb7ec458ff40
JoseH-Git <joseh.sierra@outlook.com>
2021-05-22 20:58:29 -0500

    Table_schema update

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+
+
+CREATE TABLE "salaries" (
+    "emp_no" INT PRIMARY KEY NOT NULL,
+    "salary" float NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR PRIMARY KEY NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR PRIMARY KEY NOT NULL,
+    "title" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT PRIMARY KEY  NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" VARCHAR   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+	FOREIGN KEY ("emp_no") REFERENCES salaries("emp_no"),
+	FOREIGN KEY ("emp_title_id") REFERENCES titles("title_id")
+);
+
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR NOT NULL,
+    "emp_no" INT NOT NULL,
+	primary key (dept_no,emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE "dept_emp" (
+    "dept_no" VARCHAR PRIMARY KEY NOT NULL,
+    "emp_no" INT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
